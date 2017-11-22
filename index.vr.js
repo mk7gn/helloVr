33af1e60c4a0ba75f7a05b439bfd11a8d71b0ee5
m.goncharov <m.goncharov@bossgs.org>
2017-11-22 16:20:22 +0200

    text text

--- index.vr.js
+++ index.vr.js
@@ -5,13 +5,20 @@ import {
   Pano,
   Text,
   View,
+    Cylinder,
 } from 'react-vr';
 
 export default class welcomeVr extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {textColor: 'white'};
+    }
+
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano source={asset('pano.jpg')}/>
         <Text
           style={{
             backgroundColor: '#777879',
@@ -22,10 +29,59 @@ export default class welcomeVr extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
+              color: this.state.textColor,
             transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
+          }}
+            onEnter={() => this.setState({textColor: 'red'})}
+            onExit={() => this.setState({textColor: 'white'})}
+            onMove={() => this.setState({textColor: 'orange'})}>
+          hello World
         </Text>
+          <Cylinder
+              radiusTop={0.5}
+              radiusBottom={0.5}
+              dimHeight={2}
+              segments={12}
+          />
+          <Text
+              style={{
+                  backgroundColor: '#917802',
+                  fontSize: 1,
+                  fontWeight: '600',
+                  layoutOrigin: [0.5,1.5],
+                  paddingLeft: 0.5,
+                  paddingRight: 0.5,
+                  textAlign: 'center',
+                  textAlignVertical: 'center',
+                  transform: [{translate: [0, 0, 5]}, {rotateY: 180}],
+              }}>
+              hello World
+          </Text>
+          <View style={{
+              flex: 1,
+              flexDirection: 'column',
+              width: 2,
+              alignItems: 'stretch',
+              transform: [{translate: [-1, 1, -5]}],
+          }}>
+
+              <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
+                  <Text style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
+              </View>
+              <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
+                  <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
+              </View>
+              <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
+                  <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
+              </View>
+              <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
+                  <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
+              </View>
+              <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
+                  <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
+              </View>
+
+          </View>
       </View>
     );
   }
