29db82191c2b454090c1f8e182cc987cc9e50159
m.goncharov <m.goncharov@bossgs.org>
2017-11-23 13:20:32 +0200

    fix

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,5 @@
 import React, {Component, Style} from 'react';
-import {View, Text, VrButton, Pano, asset} from 'react-vr'
+import {View, Text, VrButton, Pano, asset, StyleSheet} from 'react-vr'
 import PropTypes from 'prop-types';
 
 class Welcome extends Component {
@@ -8,25 +8,32 @@ class Welcome extends Component {
         return (
             <View>
                 <Text
-                    style={{
-                        backgroundColor  : 'transparent',
-                        fontSize         : 0.8,
-                        fontWeight       : '400',
-                        layoutOrigin     : [0.5, 0.5],
-                        paddingLeft      : 0.2,
-                        paddingRight     : 0.2,
-                        textAlign        : 'center',
-                        textAlignVertical: 'center',
-                        color            : '#eaeaea',
-                        shadowColor: 'red',
-                        transform        : [{translate: [0, -0.7, -6]}],
-                    }}>
+                    style={styles.text}>
                     Welcome to VR places!
                 </Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+   wrapper: {
+       color: 'red'
+   },
+   text: {
+       backgroundColor  : 'transparent',
+       fontSize         : 0.8,
+       fontWeight       : '400',
+       layoutOrigin     : [0.5, 0.5],
+       paddingLeft      : 0.2,
+       paddingRight     : 0.2,
+       textAlign        : 'center',
+       textAlignVertical: 'center',
+       color            : '#eaeaea',
+       shadowColor: 'red',
+       transform        : [{translate: [0, -0.7, -6]}],
+   }
+});
 
 Welcome.propTypes = {};
 Welcome.defaultProps = {};
